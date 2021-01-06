9d140f5af3267f2755fc78db13992f9733d8eb39
sdsarchit <47117871+sdsarchit@users.noreply.github.com>
2021-01-06 19:08:01 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 // Hello.java
-public class Hello {
+public class Hello {//commited archit
    public static void main(String[] args) {
       System.out.println("Hello, world from GIT!");
       System.out.println("Changes after First commit!");
